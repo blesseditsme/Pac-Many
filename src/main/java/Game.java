bdcc4844cdf1716b0e125288afd6f998da6b8985
blesseditsme <yan.tsyvunchyk@gmail.com>
2023-07-26 18:41:12 +0300

    Trying and trying

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Game {
+import javax.swing.*;
+
+public class Game extends JFrame {
+
+    Game() {
+        add(new Panel());
+    }
+    public static void main(String[] args) {
+        Game pacmanGame = new Game();
+        pacmanGame.setVisible(true);
+        pacmanGame.setTitle("Pacman");
+        pacmanGame.setSize(380,420);
+        pacmanGame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        pacmanGame.setLocationRelativeTo(null);
+    }
 }
